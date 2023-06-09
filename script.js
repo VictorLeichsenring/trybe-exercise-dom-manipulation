a2d71a0270c7c483187a8fb87122cf8b630b5aa8
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-06-09 20:16:41 -0300

    Incluída a visão em 2 anos

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 // Seu código aqui
-//
+let vision = document.getElementsByTagName('p')
+vision[1].innerText = 'Em dois anos estarei atuando na área de desenvolvimento, com o objtivo de progredir na carreira e se destacar no mercado de trabalho.';
