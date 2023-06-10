935b750604af34780ff1d94f648320185f5f043f
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-06-10 14:30:49 -0300

    Correção do título

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 const vision = document.getElementsByTagName('p');
 vision[1].innerText = 'Estar em destaque no mercado de trabalho.';
 const amarelo = document.getElementsByClassName('main-content');
-console.log(amarelo);
 amarelo[0].style.backgroundColor = 'rgb(76, 164, 109)';
 const branco = document.getElementsByClassName('center-content');
 branco[0].style.backgroundColor = 'white';
+const title = document.getElementsByClassName('title');
+title[0].innerText = 'Desafio - JavaScript';
