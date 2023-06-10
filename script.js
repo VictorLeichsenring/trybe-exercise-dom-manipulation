44f5109ff7c11ffaa42f6a49d213f91c6a794a1a
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-06-10 14:22:02 -0300

    Alteração de cor do fundo main-content

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 // Seu código aqui
 const vision = document.getElementsByTagName('p');
 vision[1].innerText = 'Estar em destaque no mercado de trabalho.';
+const amarelo = document.getElementsByClassName('main-content');
+console.log(amarelo);
+amarelo[0].style.backgroundColor = 'rgb(76, 164, 109)';
